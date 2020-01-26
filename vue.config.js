27b34829d53969fa4fe0f49b7579dedf323f104e
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2020-01-26 16:53:56 +0530

    fixed scss issue

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,17 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vendor-system/'
     : '/',
-    
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: [
+            path.resolve(__dirname, 'src/')
+          ],
+          indentedSyntax: true,
+        },
+        prependData: '@import "~@/custom.scss"',
+      },
+    }
+  }
 }
