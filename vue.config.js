93335dcf3f7cf7f5c0cd0015113b368283b2ddf3
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2020-01-26 16:44:52 +0530

    fixed issue

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./docs"),
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vendor-system/'
-    : '/'
+    : '/',
+    
 }
